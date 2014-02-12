cfcc553256b3d7734b67f4a9764fd46a82e5ef9f
Daehee Han <bookdori81@gmail.com>
2014-02-11 18:02:26 -0800

    Using Reset password thread

--- src/com/booknara/deviceadmin/DeviceAdminDemoReceiver.java
+++ src/com/booknara/deviceadmin/DeviceAdminDemoReceiver.java
@@ -5,6 +5,7 @@ import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
@@ -17,9 +18,15 @@ import android.util.Log;
  */
 public class DeviceAdminDemoReceiver extends DeviceAdminReceiver {
 	private static final String CNAME = DeviceAdminDemoReceiver.class.getSimpleName();
-	
 	private static final String SETTING_PACKAGE = "com.android.settings";
+	
+	/* Allow other admins to change the password again without entering it. */
+	private static final int RESET_PASSWORD_NOT_REQUIRE_ENTRY = 0;
+    private static int RESET_PASSWORD_TIME_OUT = 5 * 1000;
+    private static String TEMP_PASSWORD = "1";
 
+	private DevicePolicyManager mDevicepolicymanager;
+	
 	/** Called when this application is approved to be a device administrator. */
 	@Override
 	public void onEnabled(Context context, Intent intent) {
@@ -33,26 +40,27 @@ public class DeviceAdminDemoReceiver extends DeviceAdminReceiver {
         context.startActivity(intent);
     }
 
-    private void lockScreen(Context context, SharedPreferences sharedpreferences) {
-        DevicePolicyManager devicepolicymanager = (DevicePolicyManager)context.getSystemService(Context.DEVICE_POLICY_SERVICE);
-        // TODO Using sharedpreferences 
+    private void resetPassword(Context context, SharedPreferences sharedpreferences) {
+    	mDevicepolicymanager = (DevicePolicyManager)context.getSystemService(Context.DEVICE_POLICY_SERVICE);
     	// Setting reset system password
-        devicepolicymanager.resetPassword("1234", DevicePolicyManager.RESET_PASSWORD_REQUIRE_ENTRY);
-        devicepolicymanager.setPasswordExpirationTimeout(DeviceAdminUtil.getComponentName(), 30 * 1000);
-        devicepolicymanager.lockNow();
+    	mDevicepolicymanager.resetPassword(TEMP_PASSWORD, RESET_PASSWORD_NOT_REQUIRE_ENTRY);
+    	mDevicepolicymanager.lockNow();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            	mDevicepolicymanager.resetPassword("", 0);
+            }
+        }, RESET_PASSWORD_TIME_OUT);
     }
     
 	@Override
     public CharSequence onDisableRequested(Context context, Intent intent) {
-		final Context ctx = context;
         final SharedPreferences sharedpreferences = PreferenceManager.getDefaultSharedPreferences(context);
         // TODO Sending Email Service
-//        openPackageName(ctx, SETTING_PACKAGE);
-        openPackageName(context, context.getApplicationContext().getPackageName());
-        lockScreen(context, sharedpreferences);
+        openPackageName(context, SETTING_PACKAGE);
+        resetPassword(context, sharedpreferences);
         
-		Log.d(CNAME, "Disabling Device Administrator means your child change and uninstall ScreenTime.");
-        return "";
+        return "Disabling Device Administrator means your child could change and uninstall ScreenTime.";
     }
 	
 	/** Called when this application is no longer the device administrator. */
